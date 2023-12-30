2ae1171eb778bbb95b2abcc3540d342c13be318d
Jonathan Bossenger <jonathanbossenger@gmail.com>
2023-12-30 22:46:51 +0200

    Adding collision explosion and a timer

--- build/render.php
+++ build/render.php
@@ -20,14 +20,16 @@ if (function_exists('gutenberg_enqueue_module')) {
 $icon_url = WP_PLUGIN_URL . '/wp-interactive-game/icons/';
 ?>
 <div>
-    <p>Instructions: click on the WordPress logo, then use the arrow keys to move it.</p>
+    <p>Instructions: Dodge the enemies and see how long you can stay free.</p>
     <ol>
-        <li>Click on the WordPress logo.</li>
+        <li>Click "Start Game" to begin.</li>
         <li>Use the arrow keys to move the logo.</li>
-        <li>Click the "Stop Game" button to end the game loop.</li>
+        <li>Click "Reset Game" to start over.</li>
+        <li>Click "Stop Game" to end the game.</li>
     </ol>
 </div>
 <div <?php echo get_block_wrapper_attributes(); ?> data-wp-interactive='{ "namespace": "wp-interactive-game" }'>
+    <div>Time: <span id="time" data-miliseconds="0">0</span> seconds</div>
     <div tabindex="0" class="game-container" data-wp-on--keydown="actions.moveIcon" data-wp-on--keyup="actions.stopIcon" id="<?php echo esc_attr( $unique_id ); ?>">
         <img data-wp-on--click="actions.startGame" class="game-icon" style="left: 275px; top: 275px;" src="<?php echo $icon_url ?>wordpress.svg" alt="Game Icon"/>
         <img id="wix" class="enemy-icon" style="display: none; left: 0px; top:0px;" src="<?php echo $icon_url ?>wix.svg" alt="Wix: Icon"/>
@@ -39,6 +41,8 @@ $icon_url = WP_PLUGIN_URL . '/wp-interactive-game/icons/';
     </div>
 </div>
 <div data-wp-interactive='{ "namespace": "wp-interactive-game-controls" }' >
+    <button id="start-game" data-wp-on--click="actions.startGame">Start Game</button>
+    <button id="reset-game" data-wp-on--click="actions.resetGame">Reset Game</button>
     <button id="stop-game" data-wp-on--click="actions.stopGame">Stop Game</button>
 </div>
 
--- src/view.js
+++ src/view.js
@@ -128,23 +128,50 @@ function moveEnemies(delta) {
 }
 
 function detectCollision() {
+    let iconOffset = getOffset(icon);
+
     for (let i = 0; i < enemies.length; i++) {
         let enemyElement = document.querySelector(`#${enemies[i].id}`);
         let enemyOffset = getOffset(enemyElement);
-        let iconOffset = getOffset(icon);
 
-        if (iconOffset.left < enemyOffset.left + enemyElement.offsetWidth &&
-            iconOffset.left + icon.offsetWidth > enemyOffset.left &&
-            iconOffset.top < enemyOffset.top + enemyElement.offsetHeight &&
-            icon.offsetHeight + iconOffset.top > enemyOffset.top) {
+        if (iconOffset.left < enemyOffset.left + (enemyElement.offsetWidth/1.1) &&
+            iconOffset.left + (icon.offsetWidth/1.1) > enemyOffset.left &&
+            iconOffset.top < enemyOffset.top + (enemyElement.offsetHeight/1.1) &&
+            (icon.offsetHeight/1.1) + iconOffset.top > enemyOffset.top) {
             console.log('Collision detected: Game Over');
+            killer = enemies[i].id;
+            gameState = 'game-over';
         }
     }
 }
 
+function renderExplosion() {
+    icon.src = icon.src.replace('wordpress.svg', 'bomb-explosion.svg');
+    icon.style.left = `${parseInt(icon.style.left) - 50}px`;
+    icon.style.width = '150px';
+    icon.style.top = `${parseInt(icon.style.top) - 50}px`;
+    icon.style.height = '150px';
+    // hide killer enemy
+    const killerEnemy = document.querySelector(`#${killer}`);
+    killerEnemy.style.display = 'none';
+}
+
+function updateTime(progress) {
+    const timeElement = document.querySelector('#time');
+    // get data-miliseconds attribute from time element
+    const time = parseInt(timeElement.getAttribute('data-miliseconds'));
+    const newTime = time + progress;
+    // update data-miliseconds attribute
+    timeElement.setAttribute('data-miliseconds', newTime);
+    // update time element
+    timeElement.innerHTML = `${ Math.round(newTime/1000) }`;
+}
+
 // Update game state
 function update(progress) {
+    updateTime(progress);
     let delta = progress / 60;
+    updateTime(progress);
     moveIcon(delta);
     moveEnemies(delta);
     detectCollision();
@@ -160,11 +187,17 @@ function loop(timestamp) {
     }
     lastRender = timestamp
     // don't call this loop again to stop the game
-    animationFrameId = window.requestAnimationFrame(loop)
+    if (gameState === 'running') {
+        animationFrameId = window.requestAnimationFrame(loop)
+    } else {
+        renderExplosion();
+    }
 }
 
 let lastRender = 0
 let animationFrameId;
+let gameState = 'running';
+let killer;
 
 const icon = document.querySelector('.game-icon');
 const speed = 20;
@@ -247,6 +280,61 @@ store('wp-interactive-game', {
 
 store('wp-interactive-game-controls', {
     actions: {
+        startGame: () => {
+            // set focus on the game-container
+            const gameContainer = document.querySelector('.game-container');
+            gameContainer.focus();
+            // trigger click on the game-icon
+            const gameIcon = document.querySelector('.game-icon');
+            gameIcon.click();
+        },
+        resetGame: () => {
+            console.log('Reset Game');
+            const timeElement = document.querySelector('#time');
+            timeElement.innerHTML = '0';
+            timeElement.setAttribute('data-miliseconds', '0');
+            gameState = 'running';
+            icon.src = icon.src.replace('bomb-explosion.svg', 'wordpress.svg');
+            icon.style.width = '50px';
+            icon.style.height = '50px';
+            icon.style.top = '275px';
+            icon.style.left = '275px';
+            for (let i = 0; i < enemies.length; i++) {
+                const enemy = enemies[i];
+                const enemyElement = document.querySelector(`#${enemy.id}`);
+                enemyElement.style.display = 'none';
+            }
+            enemies = [
+                {
+                    id: 'wix',
+                    horizontalDirection: horizontalDirections[Math.floor(Math.random() * horizontalDirections.length)],
+                    verticalDirection: verticalDirections[Math.floor(Math.random() * verticalDirections.length)],
+                },{
+                    id: 'squarespace',
+                    horizontalDirection: horizontalDirections[Math.floor(Math.random() * horizontalDirections.length)],
+                    verticalDirection: verticalDirections[Math.floor(Math.random() * verticalDirections.length)],
+                },
+                {
+                    id: 'weebly',
+                    horizontalDirection: horizontalDirections[Math.floor(Math.random() * horizontalDirections.length)],
+                    verticalDirection: verticalDirections[Math.floor(Math.random() * verticalDirections.length)],
+                },
+                {
+                    id: 'shopify',
+                    horizontalDirection: horizontalDirections[Math.floor(Math.random() * horizontalDirections.length)],
+                    verticalDirection: verticalDirections[Math.floor(Math.random() * verticalDirections.length)],
+                },
+                {
+                    id: 'webflow',
+                    horizontalDirection: horizontalDirections[Math.floor(Math.random() * horizontalDirections.length)],
+                    verticalDirection: verticalDirections[Math.floor(Math.random() * verticalDirections.length)],
+                },
+                {
+                    id: 'contentful',
+                    horizontalDirection: horizontalDirections[Math.floor(Math.random() * horizontalDirections.length)],
+                    verticalDirection: verticalDirections[Math.floor(Math.random() * verticalDirections.length)],
+                }];
+        },
         stopGame: () => {
             console.log('Stop Game');
             window.cancelAnimationFrame(animationFrameId);
