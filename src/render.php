78c4e63090397b54e040c84cdf04edab1ba0f38f
Jonathan Bossenger <jonathanbossenger@gmail.com>
2023-12-30 21:40:53 +0200

    Enemy movement and collision detection

--- src/render.php
+++ src/render.php
@@ -29,15 +29,16 @@ $icon_url = WP_PLUGIN_URL . '/wp-interactive-game/icons/';
 </div>
 <div <?php echo get_block_wrapper_attributes(); ?> data-wp-interactive='{ "namespace": "wp-interactive-game" }'>
     <div tabindex="0" class="game-container" data-wp-on--keydown="actions.moveIcon" data-wp-on--keyup="actions.stopIcon" id="<?php echo esc_attr( $unique_id ); ?>">
-        <img data-wp-on--click="actions.startGame" class="game-icon" style="left: 0px; top: 0px;" src="<?php echo $icon_url ?>wordpress.svg" alt="Game Icon"/>
+        <img data-wp-on--click="actions.startGame" class="game-icon" style="left: 275px; top: 275px;" src="<?php echo $icon_url ?>wordpress.svg" alt="Game Icon"/>
         <img id="wix" class="enemy-icon" style="display: none; left: 0px; top:0px;" src="<?php echo $icon_url ?>wix.svg" alt="Wix: Icon"/>
         <img id="squarespace" class="enemy-icon" style="display: none; left: 0px; top:0px;" src="<?php echo $icon_url ?>squarespace.svg" alt="Squarespace: Icon"/>
         <img id="weebly" class="enemy-icon" style="display: none; left: 0px; top:0px;" src="<?php echo $icon_url ?>weebly.svg" alt="Weebly: Icon"/>
         <img id="shopify" class="enemy-icon" style="display: none; left: 0px; top:0px;" src="<?php echo $icon_url ?>shopify.svg" alt="Shopify: Icon"/>
         <img id="webflow" class="enemy-icon" style="display: none; left: 0px; top:0px;" src="<?php echo $icon_url ?>webflow.svg" alt="Webflow: Icon"/>
+        <img id="contentful" class="enemy-icon" style="display: none; left: 0px; top:0px;" src="<?php echo $icon_url ?>contentful.svg" alt="Contentful: Icon"/>
     </div>
 </div>
 <div data-wp-interactive='{ "namespace": "wp-interactive-game-controls" }' >
-    <button data-wp-on--click="actions.stopGame">Stop Game</button>
+    <button id="stop-game" data-wp-on--click="actions.stopGame">Stop Game</button>
 </div>
 
--- src/view.js
+++ src/view.js
@@ -3,87 +3,51 @@
  */
 import {store, getContext} from '@wordpress/interactivity';
 
-const icon = document.querySelector('.game-icon');
-const speed = 20;
+function getOffset(element) {
+    const rect = element.getBoundingClientRect();
+    return {
+        left: rect.left + window.scrollX,
+        top: rect.top + window.scrollY
+    };
+}
 
-let arrowLeft, arrowRight, arrowUp, arrowDown = false;
+function getGameContainerOffset() {
+    const gameContainer = document.querySelector('.game-container');
+    const gameContainerOffset = getOffset(gameContainer);
+    return gameContainerOffset;
+}
 
-const startPositions = [0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550];
-const horizontalDirections = ['left', 'right'];
-const verticalDirections = ['up', 'down'];
+function getRandomTop() {
+    const gameContainerOffset = getGameContainerOffset();
+    const randomTop = Math.floor(Math.random() * (gameContainerOffset.top + 600 - gameContainerOffset.top + 1)) + gameContainerOffset.top;
+    return randomTop;
+}
 
-let enemies = [
-    {
-        id: 'wix',
-        top: startPositions[Math.floor(Math.random() * startPositions.length)],
-        left: startPositions[Math.floor(Math.random() * startPositions.length)],
-        horizontalDirection: horizontalDirections[Math.floor(Math.random() * horizontalDirections.length)],
-        verticalDirection: verticalDirections[Math.floor(Math.random() * verticalDirections.length)],
-        downLimit: 745,
-        upLimit: -305,
-        leftLimit: -245,
-        rightLimit: 790,
-    },{
-        id: 'squarespace',
-        top: startPositions[Math.floor(Math.random() * startPositions.length)],
-        left: startPositions[Math.floor(Math.random() * startPositions.length)],
-        horizontalDirection: horizontalDirections[Math.floor(Math.random() * horizontalDirections.length)],
-        verticalDirection: verticalDirections[Math.floor(Math.random() * verticalDirections.length)],
-        downLimit: 695,
-        upLimit: -355,
-        leftLimit: -245,
-        rightLimit: 790,
-    },
-    {
-        id: 'weebly',
-        top: startPositions[Math.floor(Math.random() * startPositions.length)],
-        left: startPositions[Math.floor(Math.random() * startPositions.length)],
-        horizontalDirection: horizontalDirections[Math.floor(Math.random() * horizontalDirections.length)],
-        verticalDirection: verticalDirections[Math.floor(Math.random() * verticalDirections.length)],
-        downLimit: 645,
-        upLimit: -405,
-        leftLimit: -245,
-        rightLimit: 790,
-    },
-    {
-        id: 'shopify',
-        top: startPositions[Math.floor(Math.random() * startPositions.length)],
-        left: startPositions[Math.floor(Math.random() * startPositions.length)],
-        horizontalDirection: horizontalDirections[Math.floor(Math.random() * horizontalDirections.length)],
-        verticalDirection: verticalDirections[Math.floor(Math.random() * verticalDirections.length)],
-        downLimit: 595,
-        upLimit: -455,
-        leftLimit: -245,
-        rightLimit: 790,
-    },
-    {
-        id: 'webflow',
-        top: startPositions[Math.floor(Math.random() * startPositions.length)],
-        left: startPositions[Math.floor(Math.random() * startPositions.length)],
-        horizontalDirection: horizontalDirections[Math.floor(Math.random() * horizontalDirections.length)],
-        verticalDirection: verticalDirections[Math.floor(Math.random() * verticalDirections.length)],
-        downLimit: 545,
-        upLimit: -505,
-        leftLimit: -245,
-        rightLimit: 790,
-    }
-];
-
-let lastRender = 0
-let animationFrameId;
-
-function positionIcon(){
-    icon.style.top = `${startPositions[Math.floor(Math.random() * startPositions.length)]}px`;
-    icon.style.left = `${startPositions[Math.floor(Math.random() * startPositions.length)]}px`;
+function getRandomLeft() {
+    const gameContainerOffset = getGameContainerOffset();
+    const randomLeft = Math.floor(Math.random() * (gameContainerOffset.left + 600 - gameContainerOffset.left + 1)) + gameContainerOffset.left;
+    return randomLeft;
 }
 
 function spawnEnemies(){
-    enemies.forEach(enemy => {
+    for (let i = 0; i < enemies.length; i++) {
+
+        const enemy = enemies[i];
         const enemyElement = document.querySelector(`#${enemy.id}`);
-        enemyElement.style.top = `${enemy.top}px`;
-        enemyElement.style.left = `${enemy.left}px`;
+
+        let randomTop = getRandomTop();
+        let randomLeft = getRandomLeft()
+
+        let randomEnemyPosition = {
+            top: randomTop,
+            left: randomLeft,
+        };
+
+        enemyElement.style.top = `${randomTop}px`;
+        enemyElement.style.left = `${randomLeft}px`;
+
         enemyElement.style.display = 'block';
-    });
+    }
 }
 
 // Move the icon
@@ -117,47 +81,65 @@ function moveIcon(delta) {
 
 // Move the enemies
 function moveEnemies(delta) {
+    const gameContainerOffset = getGameContainerOffset();
 
-    const movement = 5;
+    const movement = 2;
 
     for (let i = 0; i < enemies.length; i++) {
+        let enemyElement = document.querySelector(`#${enemies[i].id}`);
 
-        const enemyElement = document.querySelector(`#${enemies[i].id}`);
+        /**
+         * Calculating left and right directions
+         */
+        let updatedLeftPosition = parseInt(enemyElement.style.left) + movement;
 
-        if (enemyElement.style.display === 'block') {
-
-            if (enemies[i].verticalDirection === 'down') {
-                if (parseInt(enemyElement.style.top) >= enemies[i].downLimit) {
-                    //verticalDirection = 'up';
-                    enemies[i].verticalDirection = 'up';
-                }
-                enemyElement.style.top = `${parseInt(enemyElement.style.top) + movement}px`;
-            }
-
-            if (enemies[i].verticalDirection === 'up') {
-                if (parseInt(enemyElement.style.top) <= enemies[i].upLimit) {
-                    enemies[i].verticalDirection = 'down';
-                }
-                enemyElement.style.top = `${parseInt(enemyElement.style.top) - movement}px`;
-            }
-
-            if (enemies[i].horizontalDirection === 'right') {
-                if (parseInt(enemyElement.style.left) >= enemies[i].rightLimit) {
-                    enemies[i].horizontalDirection = 'left';
-                }
-                enemyElement.style.left = `${parseInt(enemyElement.style.left) + movement}px`;
-            }
-
-            if (enemies[i].horizontalDirection === 'left') {
-                if (parseInt(enemyElement.style.left) <= enemies[i].leftLimit) {
-                    enemies[i].horizontalDirection = 'right';
-                }
-                enemyElement.style.left = `${parseInt(enemyElement.style.left) - movement}px`;
-            }
+        if (updatedLeftPosition >= ( gameContainerOffset.left + 550 ) ) {
+            enemies[i].horizontalDirection = 'left';
         }
 
-    }
+        if (updatedLeftPosition <= gameContainerOffset.left )  {
+            enemies[i].horizontalDirection = 'right';
+        }
 
+        /**
+         * Calculating up and down directions
+         */
+        let updatedTopPosition = parseInt(enemyElement.style.top) + movement;
+        if (updatedTopPosition >= gameContainerOffset.top + 550 ) {
+            enemies[i].verticalDirection = 'up';
+        }
+        if(updatedTopPosition <= gameContainerOffset.top) {
+            enemies[i].verticalDirection = 'down';
+        }
+
+
+        if (enemies[i].horizontalDirection === 'right') {
+            enemyElement.style.left = `${parseInt(enemyElement.style.left) + movement}px`;
+        }   else {
+            enemyElement.style.left = `${parseInt(enemyElement.style.left) - movement}px`;
+        }
+
+        if(enemies[i].verticalDirection === 'down') {
+            enemyElement.style.top = `${parseInt(enemyElement.style.top) + movement}px`;
+        }   else {
+            enemyElement.style.top = `${parseInt(enemyElement.style.top) - movement}px`;
+        }
+    }
+}
+
+function detectCollision() {
+    for (let i = 0; i < enemies.length; i++) {
+        let enemyElement = document.querySelector(`#${enemies[i].id}`);
+        let enemyOffset = getOffset(enemyElement);
+        let iconOffset = getOffset(icon);
+
+        if (iconOffset.left < enemyOffset.left + enemyElement.offsetWidth &&
+            iconOffset.left + icon.offsetWidth > enemyOffset.left &&
+            iconOffset.top < enemyOffset.top + enemyElement.offsetHeight &&
+            icon.offsetHeight + iconOffset.top > enemyOffset.top) {
+            console.log('Collision detected: Game Over');
+        }
+    }
 }
 
 // Update game state
@@ -165,14 +147,15 @@ function update(progress) {
     let delta = progress / 60;
     moveIcon(delta);
     moveEnemies(delta);
+    detectCollision();
 }
 
 // The main game loop, keeps running until the game ends.
 // https://gamedev.stackexchange.com/a/130617
 function loop(timestamp) {
-	console.log('Game Running');
     let progress = timestamp - lastRender
     if (lastRender !== timestamp) {
+        //console.log('Game Running: ' + progress + 'ms');
         update(progress);
     }
     lastRender = timestamp
@@ -180,11 +163,52 @@ function loop(timestamp) {
     animationFrameId = window.requestAnimationFrame(loop)
 }
 
+let lastRender = 0
+let animationFrameId;
+
+const icon = document.querySelector('.game-icon');
+const speed = 20;
+
+let arrowLeft, arrowRight, arrowUp, arrowDown = false;
+
+const horizontalDirections = ['left', 'right'];
+const verticalDirections = ['up', 'down'];
+
+let enemies = [
+    {
+        id: 'wix',
+        horizontalDirection: horizontalDirections[Math.floor(Math.random() * horizontalDirections.length)],
+        verticalDirection: verticalDirections[Math.floor(Math.random() * verticalDirections.length)],
+    },{
+        id: 'squarespace',
+        horizontalDirection: horizontalDirections[Math.floor(Math.random() * horizontalDirections.length)],
+        verticalDirection: verticalDirections[Math.floor(Math.random() * verticalDirections.length)],
+    },
+    {
+        id: 'weebly',
+        horizontalDirection: horizontalDirections[Math.floor(Math.random() * horizontalDirections.length)],
+        verticalDirection: verticalDirections[Math.floor(Math.random() * verticalDirections.length)],
+    },
+    {
+        id: 'shopify',
+        horizontalDirection: horizontalDirections[Math.floor(Math.random() * horizontalDirections.length)],
+        verticalDirection: verticalDirections[Math.floor(Math.random() * verticalDirections.length)],
+    },
+    {
+        id: 'webflow',
+        horizontalDirection: horizontalDirections[Math.floor(Math.random() * horizontalDirections.length)],
+        verticalDirection: verticalDirections[Math.floor(Math.random() * verticalDirections.length)],
+    },
+    {
+        id: 'contentful',
+        horizontalDirection: horizontalDirections[Math.floor(Math.random() * horizontalDirections.length)],
+        verticalDirection: verticalDirections[Math.floor(Math.random() * verticalDirections.length)],
+    }];
+
 store('wp-interactive-game', {
     actions: {
         startGame: () => {
             console.log('Start Game');
-            positionIcon();
             spawnEnemies();
             animationFrameId = window.requestAnimationFrame(loop)
         },
