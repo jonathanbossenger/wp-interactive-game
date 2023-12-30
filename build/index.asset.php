f8ea04cfeccd49f3963c64ff817c6b846ad766a9
Jonathan Bossenger <jonathanbossenger@gmail.com>
2023-12-30 22:57:44 +0200

    More Polish

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0ead8130742394c551af');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c5c64b0cb751befd0428');
